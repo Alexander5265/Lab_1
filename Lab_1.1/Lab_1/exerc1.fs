b0b4dac1d1e7e8e6d693826e41c207110946c262
Alexander5265 <104996703+Alexander5265@users.noreply.github.com>
2026-02-25 09:13:55 +0500

    Update exerc1.fs

--- Lab_1.1/Lab_1/exerc1.fs
+++ Lab_1.1/Lab_1/exerc1.fs
@@ -1,29 +1,27 @@
 ﻿open System
 
-//  Функция читает числа и накапливает модули в списке acc 
-let rec readNumbs acc =   // Acc - это аккумулятор, сюда складываем временный результат
+let rec readNumbs acc =   // Cюда складываем временный результат
     printf "Введите число (или нажмите Enter для завершения): "
     let input = Console.ReadLine()
 
     if String.IsNullOrWhiteSpace input then
         acc 
     else
-        match Double.TryParse input with   // Превращение строки в double
-        | true, number ->   // Вычисляем модуль числа
+        match Double.TryParse input with
+        | true, number ->
             let absoluteValue = abs number
             readNumbs (absoluteValue :: acc)
-        | false, _ ->   // Повторяем ввод с тем же списком acc
+        | false, _ ->
             printfn "Некорректный ввод. Попробуйте снова."
             readNumbs acc 
 
-// Точка ввода программы (ее начало)
 [<EntryPoint>] 
 let main _ =
     let absoluteValues =
-        readNumbs []    // Создание пустого списка ( |> означает "передаем результат дальше")
-        |> List.rev // Превращения списка в reversed для восстонавления правильного порядка
+        readNumbs []    
+        |> List.rev
 
     printfn "Список модулей введённых чисел:"
-    absoluteValues |> List.iter (printfn "%f") // List.iter - проход по каждому эл списка || printfn "%f" - вывод чисел float
+    absoluteValues |> List.iter (printfn "%f")
 
     0
