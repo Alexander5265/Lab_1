a4faed27a9e10cbbffac036e674b5f396c0a0645
Alexander5265 <104996703+Alexander5265@users.noreply.github.com>
2026-02-25 08:59:40 +0500

    Update exerc1.fs

--- Lab_1.1/Lab_1/exerc1.fs
+++ Lab_1.1/Lab_1/exerc1.fs
@@ -1,29 +1,29 @@
 ﻿open System
 
-(*  функция читает числа и накапливает модули в списке acc *)
-let rec readNumbs acc =   // acc - аккумулятор, сюда складываем временный результат
+//  Функция читает числа и накапливает модули в списке acc 
+let rec readNumbs acc =   // Acc - это аккумулятор, сюда складываем временный результат
     printf "Введите число (или нажмите Enter для завершения): "
     let input = Console.ReadLine()
 
     if String.IsNullOrWhiteSpace input then
-        acc   // если ввели enter, то запрос на ввод останавливается и список acc возвращается с полученными данными
+        acc 
     else
-        match Double.TryParse input with   // превращение строки в доубл
-        | true, number ->   // все норм, вычисляем модуль числа
+        match Double.TryParse input with   // Превращение строки в double
+        | true, number ->   // Вычисляем модуль числа
             let absoluteValue = abs number
-            readNumbs (absoluteValue :: acc)   // добавляем в список найденный модуль (:: добавляет в начало списка)
-        | false, _ ->   // все не норм, повторяем ввод с тем же списком acc
+            readNumbs (absoluteValue :: acc)
+        | false, _ ->   // Повторяем ввод с тем же списком acc
             printfn "Некорректный ввод. Попробуйте снова."
             readNumbs acc 
 
-(* точка ввода программы (ее начало) *)
+// Точка ввода программы (ее начало)
 [<EntryPoint>] 
 let main _ =
     let absoluteValues =
-        readNumbs []    // создаем пустой список ( |> означает "передаем результат дальше")
-        |> List.rev // превращаем список в реверсед, чтобы восстановить порядок 
+        readNumbs []    // Создание пустого списка ( |> означает "передаем результат дальше")
+        |> List.rev // Превращения списка в reversed для восстонавления правильного порядка
 
     printfn "Список модулей введённых чисел:"
-    absoluteValues |> List.iter (printfn "%f") (* List.iter - проход по каждому эл списка || printfn "%f" - вывод чисел float *)
+    absoluteValues |> List.iter (printfn "%f") // List.iter - проход по каждому эл списка || printfn "%f" - вывод чисел float
 
     0
