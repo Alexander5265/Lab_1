641f38d424892fa1c66aad9476e775f01aedc56a
Alexander5265 <104996703+Alexander5265@users.noreply.github.com>
2026-02-25 09:02:50 +0500

    Update exerc2.fs

--- Lab 1.2/Lab 1.2/exerc2.fs
+++ Lab 1.2/Lab 1.2/exerc2.fs
@@ -1,21 +1,21 @@
 ﻿open System
 
 let rec factorial n =
-    if n <= 1 then   // тк факториал 0 или 1 равен 1
+    if n <= 1 then   // Факториал 0 или 1 равен 1
         1
     else
-        n * factorial (n - 1)   // формула факториала уже с рекурсией
+        n * factorial (n - 1)   // Формула факториала уже с рекурсией
 
 [<EntryPoint>]
 let main _ =
     printf "Введите неотрицательное число: "
     let input = Console.ReadLine()
 
-    match Int32.TryParse input with // деалем из инпута число
-    | true, number when number >= 0 ->    // паттерн матчинг при тру
+    match Int32.TryParse input with // Pattern matching 
+    | true, number when number >= 0 -> 
         let result = factorial number
         printfn "Факториал числа %d равен %d" number result
-    | _ ->    // паттерн матчинг при фалсе
+    | _ ->  
         printfn "Некорректный ввод."
 
     0
